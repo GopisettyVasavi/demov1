298f903bbde245124b4e237dc49c66f439da4885
GopisettyVasavi <vasavi.vakkanti@gmail.com>
2020-07-01 15:58:05 +1000

    commit

---
+++ src/main/java/com/renaissance/util/APIConstants.java
@@ -0,0 +1,51 @@
+package com.renaissance.util;
+
+public class APIConstants {
+	public static final String CONTRACTOR_DETAILS="/contractordetails";
+	public static final String CONTRACTOR_DETAILS_CONTRACTORID="/contractordetails/{contractorId}";
+	public static final String EMPTY_REDIRECT="redirect:/";
+	public static final String REDIRECT_CONTRACTOR_DETAILS="redirect:/contractordetails";
+	public static final String UPDATE_CONTRACTOR="/updateContractor";
+	public static final String BANK_HISTORY="/bankhistory/{contractorId}";
+	public static final String ABN_HISTORY="/abnhistory/{contractorId}";
+	public static final String TFN_HISTORY="/tfnhistory/{contractorId}";
+	public static final String RATE_HISTORY="/ratehistory/{contractorId}";
+	public static final String SA_HISTORY="/sahistory/{contractorId}";
+	public static final String EMP_HISTORY="/emphistory/{contractorId}";
+	public static final String CONTRACTOR_MAIN="/contractormain";
+	public static final String MAIN_CONTRACTOR="contractormain";
+	public static final String CREATE_CONTRACTOR="/createContractor";
+	public static final String COPY_CERTIFICATE="/copyCertificate";
+	public static final String SEARCH_CONTRACTORS="/searchContractors";
+	public static final String CALCULATE_MARGIN="/calculatemargin";
+	public static final String STATE_PAYROLL_TAX="/payrolltax/{state}";
+	public static final String INSURANCE_PERCENT="/insurancePercent";
+	public static final String CONSTANTS_DEF="/constantsdef";
+	public static final String UPDATE_CONSTANTS="/updateconstants";
+	public static final String GET_CONSTANTS="/getconstants";
+	public static final String BULK_FILE_UPLOAD="/bulkfileupload";
+	public static final String BULK_UPLOAD="/bulkupload";
+	public static final String FAILED_CANDIDATE_DETAILS="/failedcandidatedetails/{fileName}";
+	public static final String ERROR="/error";
+	public static final String CANDIDATE_DETAILS="/candidatedetails";
+	public static final String CANDIDATE_DETAILS_CANDIDATE_ID="/candidatedetails/{candidateId}";
+	public static final String PROFILE_PARSER=	"/profileparser";
+	public static final String UPLOAD="/upload";
+	public static final String UPLOAD_STATUS="/uploadStatus";
+	public static final String CREATE_PROFILE="/createProfile";
+	public static final String COPY_FILE="/copyFile";
+	public static final String LOGIN="/login";
+	public static final String INDEX="/index";
+	public static final String INDEX_LOAD="/indexload";
+	public static final String LOGOUT="/logout";
+	public static final String LOGOUT_USER="/logoutuser";
+	public static final String PROFILE_SEARCH="/profilesearch";
+	public static final String SEARCH_CANDIDATES="/searchcandidates";
+	public static final String REGISTER_EMPLOYEE="/registeremployee";
+	public static final String CHANGE_PASSWORD="/changepassword";
+	public static final String SAVE_EMPLOYEE="/saveemployee";
+	public static final String PASSWORD_CHANGE="/passwordchange";
+	public static final String RECRUITER_LIST="/recruiterList";
+	
+
+}
